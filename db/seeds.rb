805901f4f5f07eb2890efa139e4ff05f31d0cad6
edouard de beauffort <e.debeauffort@gmail.com>
2017-02-15 16:11:57 +0000

    ex done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+restaurants = Restaurant.create([
+  {name: "Quick", address: "Kings Cross", phone_number: "0123456789", category: "belgian"},
+  {name: "Franco Manca", address: "Soho", phone_number: "0000000000", category: "italian"},
+  {name: "Tepaniaki", address: "chinatown", phone_number: "0987654321", category: "japanese"},
+  {name: "Clos Maggiore", address: "Soho", phone_number: "0234567890", category: "french"},
+  {name: "ping pong", address: "Angel", phone_number: "5463849725", category: "chinese"}
+  ])
